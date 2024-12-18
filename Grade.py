4e79ad62ec6c6a3cbccc20821c132e61c8d2e600
Benyamina Meriem <imeran529@gmail.com>
2024-12-18 08:51:43 +0100

    Add files via upload

---
+++ Grade.py
@@ -0,0 +1,13 @@
+score = int(input("Enter the score of the student (0-100): "))
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print(f"The student grade is: {grade}")
